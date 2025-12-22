c0c513c18c6c86760f0b44f495dbbc6a966e12a2
Vixzye <alexzvito123@gmail.com>
2025-12-22 22:02:52 +0700

    Modified data_types.py

--- Python/python-roadmap/01-basics/data_types.py
+++ Python/python-roadmap/01-basics/data_types.py
@@ -1,12 +1,38 @@
 # data_types.py
 # Demonstration of basic Python data types
 
-integer_value = 10
-float_value = 3.14
-string_value = "Python"
-boolean_value = False
+# Case 1: Type belongs to the object, not the name
+x = 10
+print(type(x))
 
-print(type(integer_value))
-print(type(float_value))
-print(type(string_value))
-print(type(boolean_value))
+x = "10"
+print(type(x))
+
+# Case 2: Immutable objects cannot be modified in place
+x = 10
+print(id(x))
+
+x = x + 1
+print(id(x))
+
+# Case 3: Mutable objects can be modified in place
+x = [1, 2, 3]
+print(id(x))
+
+x.append(4)
+print(id(x))
+
+print(x)
+
+# Case 4: Shared reference to a mutable object
+a = [1, 2]
+b = a
+
+print(id(a))
+print(id(b))
+
+b.append(3)
+
+print(a)
+print(b)
+
